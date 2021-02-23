4671aad72cb0d85dde1c4b56fbc19d2d5d590559
Matt Johnson <mattj@microsoft.com>
2021-02-23 10:04:20 -0800

    Added tests for extending the types we can parse and handle

---
+++ Testing/UnitTests/ExtendedTypes/EmojiExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Mossharbor.ActivityStreams;
+using System;
+using System.Linq;
+
+namespace Mossharbor.ActivityStreams.UnitTests
+{
+    /// <summary>
+    /// this is a test class to test adding parsing of an emoji
+    /// </summary>
+    public class EmojiExtension : ActivityObject
+    {
+        /// <summary>
+        /// the emoji type we are parsing
+        /// </summary>
+        public static string EmojiType = "Emoji";
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public EmojiExtension() : base(EmojiType) { }
+    }
+}
---
+++ Testing/UnitTests/Extensibility.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mossharbor.ActivityStreams;
+using System;
+using System.Linq;
+
+namespace Mossharbor.ActivityStreams.UnitTests
+{
+    /// <summary>
+    /// this is the tsting for the subset of json LD we support
+    /// </summary>
+    [TestClass]
+    public class Extensibility
+    {
+        /// <summary>
+        /// Testing Spec example 1
+        /// </summary>
+        [TestMethod]
+        public void AddingCustomEmojiTypes()
+        {
+            ActivityStreamsParser.TypeToObjectMap.Add(EmojiExtension.EmojiType, () => new EmojiExtension());
+
+            ActivityObjectBuilder builder = new ActivityObjectBuilder();
+            NoteObject activity = (NoteObject)builder.FromJson(System.IO.File.OpenRead(@".\Extensions\customemoji.json"))
+                            .ExpandExtentionContexts()
+                            .Build();
+
+            Assert.IsNotNull(activity.Tag[0] is EmojiExtension);
+            Assert.IsTrue(activity.ExtendedContexts.ContainsKey("toot"), "toot is missing");
+            Assert.IsTrue(activity.ExtendedContexts.ContainsKey("Emoji"), "Emoji is missing");
+            Assert.IsTrue(activity.Tag[0].Obj.Type=="Emoji", "Emoji expansion is missing");
+            Assert.IsTrue(activity.Tag[0].Obj.Name == ":kappa:", "Emoji expansion is missing");
+            Assert.IsNotNull(activity.Tag[0].Obj.Icons[0].Obj is ImageObject, "Emoji expansion is missing Images");
+        }
+
+        /// <summary>
+        /// Testing Spec example 1
+        /// </summary>
+        [TestMethod]
+        public void AddingCustomPublicKeyTypes()
+        {
+            // TODO figure out how to do this easily.
+        }
+    }
+}
